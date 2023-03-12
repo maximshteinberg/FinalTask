71831c9b65a83e80e73580be340e039e2481d846
Maxim Shteinberg <maxim518@mail.ru>
2023-03-12 19:09:08 +0300

    final

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите строки массива, разделяя запятой:"," ");
+string str = Console.ReadLine();
+string[] array = str.Split(',');
+
+
+System.Console.WriteLine();
+System.Console.WriteLine("Результат выборки: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+   
+    if (array[i].Length <= 3)
+    {
+        System.Console.Write("[" + string.Join(", ", array[i]) + "]");
+    }
+}
